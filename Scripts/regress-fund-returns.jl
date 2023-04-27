a1e7684bd73340f6b96cd1da61a5ca5e25f802b7
Jon Aster <jonaster.mail@gmail.com>
2023-04-27 15:03:45 +1000

    worked on analysis of betas

--- Scripts/regress-fund-returns.jl
+++ Scripts/regress-fund-returns.jl
@@ -3,6 +3,7 @@ using CSV
 using GLM
 using Dates
 using Base.Threads
+using JLD2
 
 include("CommonFunctions.jl")
 include("CommonConstants.jl")
@@ -12,6 +13,20 @@ using .CommonConstants
 using .DataReader
 
 const OUTPUT_FILESTRING_BASE = "./data/results/"
+const DATA_FORMATTING_DESCRIPTION = [
+    ["Regression betas (excluding constant)"],
+    ["Standard errors of currency factors only"],
+    "Degrees of freedom"
+]
+const DATA_FORMATTING_FUNCTIONS = (
+    format_coefs = regfit -> coef(regfit) |> non_constant_coefs .|> n -> round(n, digits=5),
+    format_se = regfit -> stderror(regfit) |> currency_coefs .|> n -> round(n, digits=5),
+    format_df = regfit -> dof_residual(regfit)
+)
+const DATA_FORMATTING_TYPE = Tuple{Vector{Float64}, Vector{Float64}, Float64}
+
+non_constant_coefs(coef_vector) = coef_vector[2:end]
+currency_coefs(coef_vector) = coef_vector[end-2:end]
 
 function compute_timevarying_betas(regression_table; id_col, date_col, y, X)
     regression_results = group_transform(
@@ -31,7 +46,7 @@ function timevarying_regressions(date, y, X_cols...)
     first_beta_date = offset_monthend(data_start_date, DEFAULT_BETA_LAGS)
     first_beta_index = findfirst(>=(first_beta_date), date)
 
-    result = Vector{Union{Missing, NamedTuple}}(fill(missing, datasize))
+    result = Vector{Union{Missing, DATA_FORMATTING_TYPE}}(fill(missing, datasize))
     isnothing(first_beta_index) && return result
     for i in first_beta_index:datasize
         sub_start_date = offset_monthend(date[i], -DEFAULT_BETA_LAGS)
@@ -46,11 +61,11 @@ function timevarying_regressions(date, y, X_cols...)
 
         regfit = lm(complete_sub_X, complete_sub_y)
 
-        v_coef = coef(regfit)
-        v_se = stderror(regfit)
-        v_df = dof_residual(regfit)
+        v_coef = DATA_FORMATTING_FUNCTIONS.format_coefs(regfit)
+        v_se = DATA_FORMATTING_FUNCTIONS.format_se(regfit)
+        v_df = DATA_FORMATTING_FUNCTIONS.format_df(regfit)
 
-        result[i] = (coef=v_coef, se=v_se, df=v_df)
+        result[i] = (v_coef, v_se, v_df)
     end
     return result
 end
@@ -75,9 +90,6 @@ function main(options_folder)
             full_data; id_col=:fundid, date_col=:date, y=:ret, X=complete_factors
         )
 
-        add_regressornames(r) = ismissing(r) ? missing : (regressors=complete_factors, r...)
-        model_results = map(add_regressornames, model_results)
-
         lock(results_lock) do
             full_results[!, model_name] = model_results
         end
@@ -92,13 +104,19 @@ function main(options_folder)
 
     dropmissing!(full_results)
 
+    println("Saving results...")
     output_folderstring = joinpath(OUTPUT_FILESTRING_BASE, options_folder)
     if !isdir(output_folderstring)
         mkdir(output_folderstring)
     end
 
-    output_filestring = joinpath(output_folderstring, "betas.csv")
-    CSV.write(output_filestring, full_results)
+    output_filestring = joinpath(output_folderstring, "betas.jld2")
+    save(
+        output_filestring, Dict(
+            "data"=>full_results,
+            "data_description"=>DATA_FORMATTING_DESCRIPTION
+        )
+    )
 
     time_duration = round(time() - time_start, digits=2)
     println(
