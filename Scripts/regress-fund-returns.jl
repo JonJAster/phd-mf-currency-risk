79f812803ad1f9df0f0492eb3e96d9d1a22f4422
Jon Aster <jonaster.mail@gmail.com>
2023-05-02 14:07:28 +1000

    fixed output data form for regressions

--- Scripts/regress-fund-returns.jl
+++ Scripts/regress-fund-returns.jl
@@ -3,7 +3,7 @@ using CSV
 using GLM
 using Dates
 using Base.Threads
-using JLD2
+using Arrow
 
 include("CommonFunctions.jl")
 include("CommonConstants.jl")
@@ -13,61 +13,63 @@ using .CommonConstants
 using .DataReader
 
 const OUTPUT_FILESTRING_BASE = "./data/results/"
-const DATA_FORMATTING_DESCRIPTION = [
-    ["Regression betas (excluding constant)"],
-    ["Standard errors of currency factors only"],
-    "Degrees of freedom"
-]
-const DATA_FORMATTING_FUNCTIONS = (
-    format_coefs = regfit -> coef(regfit) |> non_constant_coefs .|> n -> round(n, digits=5),
-    format_se = regfit -> stderror(regfit) |> currency_coefs .|> n -> round(n, digits=5),
-    format_df = regfit -> dof_residual(regfit)
-)
-const DATA_FORMATTING_TYPE = Tuple{Vector{Float64}, Vector{Float64}, Float64}
+const REGRESSION_OUTPUT_COLS = [:coef, :se, :df]
 
-non_constant_coefs(coef_vector) = coef_vector[2:end]
-currency_coefs(coef_vector) = coef_vector[end-2:end]
+const PERFECT_MULTIPLE = 0
+
+@inline currency_coefs(coef_vector) = coef_vector[end-2:end]
+@inline combine_reg_results(date, rr...) = zip(repeat(date, inner=size(first(rr),1)), vec.(rr)...)
 
 function compute_timevarying_betas(regression_table; id_col, date_col, y, X)
-    regression_results = group_transform(
+    regression_results = group_combine(
         regression_table, id_col,
-        [date_col, y, X...], timevarying_regressions, :results
+        [date_col, y, X...], timevarying_regressions, [date_col, REGRESSION_OUTPUT_COLS...],
+        cast=false
     )
 
-    return regression_results.results
+    regressor_list = vcat(:const, X...)
+    n_repeats, fit_check = divrem(size(regression_results, 1), length(regressor_list))
+    @assert fit_check == PERFECT_MULTIPLE
+    regression_results.factor = repeat(regressor_list, n_repeats)
+
+    return regression_results[:, [id_col, date_col, :factor, REGRESSION_OUTPUT_COLS...]]
 end
 
 function timevarying_regressions(date, y, X_cols...)
-    datasize = length(y)
+    n_obs = length(y)
     X_no_constant = reduce(hcat, X_cols)
-    X = hcat(ones(datasize), X_no_constant)
-    
+    X = hcat(ones(n_obs), X_no_constant)
+    n_factors = size(X, 2)
+
     data_start_date = first(date)
     first_beta_date = offset_monthend(data_start_date, DEFAULT_BETA_LAGS)
     first_beta_index = findfirst(>=(first_beta_date), date)
 
-    result = Vector{Union{Missing, DATA_FORMATTING_TYPE}}(fill(missing, datasize))
-    isnothing(first_beta_index) && return result
-    for i in first_beta_index:datasize
-        sub_start_date = offset_monthend(date[i], -DEFAULT_BETA_LAGS)
-        sub_start_index = findfirst(>=(sub_start_date), date)
-        sub_y = y[sub_start_index:i]
-        length(sub_y) <= DEFAULT_MIN_REGRESSION_OBS && continue
-        sub_X = X[sub_start_index:i, :]
+    coef_out = Matrix{Union{Missing, Float64}}(missing, n_factors, n_obs)
+    se_out = Matrix{Union{Missing, Float64}}(missing, n_factors, n_obs)
+    df_out = Matrix{Union{Missing, Float64}}(missing, n_factors, n_obs)
 
-        nonmissing_y = findall(!ismissing, sub_y)
-        complete_sub_y = Vector{Float64}(sub_y[nonmissing_y])
-        complete_sub_X = sub_X[nonmissing_y, :]
+    if !isnothing(first_beta_index)
+		@threads for i in first_beta_index:n_obs
+			sub_start_date = offset_monthend(date[i], -DEFAULT_BETA_LAGS)
+			sub_start_index = findfirst(>=(sub_start_date), date)
+			sub_y = y[sub_start_index:i]
+			length(sub_y) <= DEFAULT_MIN_REGRESSION_OBS && continue
+			sub_X = X[sub_start_index:i, :]
 
-        regfit = lm(complete_sub_X, complete_sub_y)
+			nonmissing_y = findall(!ismissing, sub_y)
+			complete_sub_y = Vector{Float64}(sub_y[nonmissing_y])
+			complete_sub_X = sub_X[nonmissing_y, :]
 
-        v_coef = DATA_FORMATTING_FUNCTIONS.format_coefs(regfit)
-        v_se = DATA_FORMATTING_FUNCTIONS.format_se(regfit)
-        v_df = DATA_FORMATTING_FUNCTIONS.format_df(regfit)
+			regfit = lm(complete_sub_X, complete_sub_y)
 
-        result[i] = (v_coef, v_se, v_df)
-    end
-    return result
+			coef_out[:, i] = coef(regfit)
+			se_out[:, i] = stderror(regfit)
+			df_out[:, i] = [dof_residual(regfit) for _ in 1:n_factors]
+		end
+	end
+    
+    return combine_reg_results(date, coef_out, se_out, df_out)
 end
 
 function main(options_folder)
@@ -76,52 +78,49 @@ function main(options_folder)
     full_data = initialise_main_data(options_folder)
     
     println("Running regressions...")
-    full_results = copy(full_data[:, [:fundid, :date]])
-    results_lock = ReentrantLock()
+    model_results = Dict(model=>full_data[:, [:fundid, :date]] for model in COMPLETE_MODELS)
 
-    @threads for (currency_risk_model, benchmark_model) in COMPLETE_MODELS
+    @threads for model in COMPLETE_MODELS
         process_start = time()
-        benchmark_factors = BENCHMARK_MODELS[benchmark_model]
-        currency_risk_factors = CURRENCYRISK_MODELS[currency_risk_model]
+        currency_risk_model_name, benchmark_model_name = model
+        benchmark_factors = BENCHMARK_MODELS[benchmark_model_name]
+        currency_risk_factors = CURRENCYRISK_MODELS[currency_risk_model_name]
         complete_factors = vcat(benchmark_factors, currency_risk_factors)
-        model_name = Symbol("$(benchmark_model)_$(currency_risk_model)_betas")
         
-        model_results = compute_timevarying_betas(
+        model_result = compute_timevarying_betas(
             full_data; id_col=:fundid, date_col=:date, y=:ret, X=complete_factors
         )
 
-        lock(results_lock) do
-            full_results[!, model_name] = model_results
-        end
+        dropmissing!(model_result)
 
-        process_elapsed = round(time() - process_start, digits=2)
+        model_results[model] = model_result
+        
+        process_elapsed_s = round(time() - process_start, digits=2)
+        process_elapsed_m = round(process_elapsed_s/60, digits=2)
         println(
-            "Process finished regressing on $(benchmark_model) with " *
-            "$(currency_risk_model) in $process_elapsed seconds ($(process_elapsed/60) " *
-            "minutes))"
+            "Process finished regressing on $(benchmark_model_name) with " *
+            "$(currency_risk_model_name) in $process_elapsed_s seconds " *
+            "($process_elapsed_m minutes)"
         )
     end
 
-    dropmissing!(full_results)
-
     println("Saving results...")
-    output_folderstring = joinpath(OUTPUT_FILESTRING_BASE, options_folder)
+    output_folderstring = joinpath(OUTPUT_FILESTRING_BASE, options_folder, "betas")
+
     if !isdir(output_folderstring)
         mkdir(output_folderstring)
     end
 
-    output_filestring = joinpath(output_folderstring, "betas.jld2")
-    save(
-        output_filestring, Dict(
-            "data"=>full_results,
-            "data_description"=>DATA_FORMATTING_DESCRIPTION
-        )
-    )
+    for (currency_risk, benchmark) in COMPLETE_MODELS
+		filestring = joinpath(output_folderstring, "$(benchmark)_$currency_risk.arrow")
+        Arrow.write(filestring, model_results[(currency_risk, benchmark)])
+    end
 
-    time_duration = round(time() - time_start, digits=2)
+    time_duration_s = round(time() - time_start, digits=2)
+    time_duration_m = round(time_duration_s/60, digits=2)
     println(
-        "Finished fund regressions in $time_duration seconds " *
-        "($(time_duration/60) minutes)"
+        "Finished fund regressions in $time_duration_s seconds " *
+        "($time_duration_m minutes)"
     )
 end
 
