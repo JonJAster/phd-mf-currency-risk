d003eea24b9cb12fc0a23f3b3cf62cec9f16e041
Jon Aster <jonaster.mail@gmail.com>
2023-11-13 14:41:27 +1000

    update test script

--- src/scratch-testing.jl
+++ src/scratch-testing.jl
@@ -3,10 +3,31 @@ using CSV
 using DataFrames
 
 function test()
-    df = Arrow.Table("data/equities/factor-series/unhedged_global_mkt.arrow") |> DataFrame
-    sort(df, [:currency, :date])
-    display(df_csv[1:20,[1,2,3,320,321,322,323,324,325]])
-    println(df)
+    df = CSV.read("data/mutual-funds/post-processing/usd-rets_na-int_eq-strict_targets_age-filtered/initialised/mf_usa.csv", DataFrame)
+    df_unfiltered = CSV.read("data/mutual-funds/post-processing/usd-rets_na-int_eq-strict_targets/initialised/mf_usa.csv", DataFrame)
+    df = dropmissing(df, [:ret_gross_m, :fund_flow])
+    df_unfiltered = dropmissing(df_unfiltered, [:ret_gross_m, :fund_flow])
 
-    println(names(df)[4])
+    obs_filt = size(df, 1)
+    obs_all = size(df_unfiltered, 1)
+
+    funds_filt = length(unique(df.fundid))
+    funds_all = length(unique(df_unfiltered.fundid))
+
+    println("nonmissing observations for all age funds: $obs_all")
+    println("nonmissing observations for age-filtered funds: $obs_filt")
+    println()
+    println("unique funds without filtering: $funds_all")
+    println("unique age-filtered funds: $funds_filt")
+    println()
+
+    println(
+        "obs filtered out: $(obs_all - obs_filt) "*
+        "($(round(100 * (obs_all - obs_filt) / obs_all, digits=2))%)"
+    )
+
+    println(
+        "funds filtered out: $(funds_all - funds_filt) "*
+        "($(round(100 * (funds_all - funds_filt) / funds_all, digits=2))%)"
+    )
 end
