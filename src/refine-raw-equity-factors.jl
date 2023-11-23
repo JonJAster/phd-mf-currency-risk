fdeabcf59180c952bf64b04b7f9602f00a4f974f
Jon Aster <jonaster.mail@gmail.com>
2023-11-23 14:11:28 +1000

    Refactor equity factor code to include market data

--- src/refine-raw-equity-factors.jl
+++ src/refine-raw-equity-factors.jl
@@ -1,43 +1,58 @@
-using
-    DataFrames,
-    CSV,
-    Arrow
+using DataFrames
+using CSV
+using Arrow
 
 include("shared/CommonConstants.jl")
 include("shared/CommonFunctions.jl")
-using
-    .CommonFunctions,
-    .CommonConstants
+using .CommonFunctions
+using .CommonConstants
 
 const INPUT_DIR = joinpath(DIRS.equity, "raw/factors")
+const INPUT_DIR_RISKFREE = joinpath(DIRS.equity, "raw")
 const OUTPUT_DIR = joinpath(DIRS.equity, "factor-series")
 
-const FACTOR_LIST = ["smb", "hml", "cma", "rmw", "wml"]
-const READ_COLUMNS = [:date, :ret]
+const GLOBAL_FACTOR_LIST = ["smb", "hml", "cma", "rmw", "wml"]
+const GLOBAL_READ_COLUMNS = [:date, :ret]
+const USA_FACTOR_LIST = ["mkt", "smb", "hml", "wml"]
 
 function main()
     time_start = time()
 
     println("Reading data...")
-    factors_list = read_factors_data()
+    global_factors_list = read_global_factors_data()
+
+    global_mkt_filename = joinpath(INPUT_DIR, "global_mkt.csv")
+    global_mkt = CSV.read(global_mkt_filename, DataFrame)
+
+    usa_filename = joinpath(INPUT_DIR, "usa_factors.csv")
+    usa_factors = CSV.read(usa_filename, DataFrame)
+
+    riskfree_filename = joinpath(INPUT_DIR_RISKFREE, "usd_riskfree.csv")
+    riskfree_data = CSV.read(riskfree_filename, DataFrame)
 
     println("Combining data...")
-    combined_factors = reduce((x,y)->innerjoin(x, y, on=:date), factors_list)
-    
-    output_filestring = makepath(OUTPUT_DIR, "equity_factors.arrow")
+    complete_global_list = [global_factors_list..., global_mkt, riskfree_data]
+    global_factors = reduce((x,y)->innerjoin(x, y, on=:date), complete_global_list)
 
-    Arrow.write(output_filestring, combined_factors)
+    usa_factors = innerjoin(usa_factors, riskfree_data, on=:date)
+    
+    output_filestring_global = makepath(OUTPUT_DIR, "global_equity_factors.arrow")
+    output_filestring_usa = makepath(OUTPUT_DIR, "usa_equity_factors.arrow")
+
+    println("Writing data...")
+    Arrow.write(output_filestring_global, global_factors)
+    Arrow.write(output_filestring_usa, usa_factors)
 
     time_duration = round(time() - time_start, digits=2)
     println("Finished combining equity factors in $time_duration seconds")
 end
 
-function read_factors_data()
+function read_global_factors_data()
     factors_list = []
 
-    for factor_code in FACTOR_LIST
+    for factor_code in GLOBAL_FACTOR_LIST
         filestring = joinpath(INPUT_DIR, "global_$factor_code.csv")
-        factor_data = CSV.read(filestring, DataFrame, select=READ_COLUMNS)
+        factor_data = CSV.read(filestring, DataFrame, select=GLOBAL_READ_COLUMNS)
         rename!(factor_data, :ret => Symbol(factor_code))
         push!(factors_list, factor_data)
     end
