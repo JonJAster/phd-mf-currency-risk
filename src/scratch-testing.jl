9a6cfcba9a6ed37ac1a9b9b943f87240e6f33c47
Jon Aster <jonaster.mail@gmail.com>
2024-03-29 09:37:31 +1000

    test count funds

--- src/scratch-testing.jl
+++ src/scratch-testing.jl
@@ -26,38 +26,13 @@ function test()
     info_fundids = info.FundId |> Set
     info_secids = info.SecId |> Set
 
-    data_fundids = Set{String}()
-    data_secids = Set{String}()
-    for file in filepaths # file = filepaths[2]
-        occursin("info", file) && continue
-        file_data = CSV.read(file, DataFrame)
-        file_fundids = file_data.FundId |> Set
-        file_secids = file_data.SecId |> Set
-
-        if !isempty(data_fundids)
-            difference = setdiff(file_fundids, data_fundids)
-            if !isempty(difference)
-                println("fundids don't match")
-                println(file)
-                println(difference)
-                println()
-            end
-        end
-        if !isempty(data_secids)
-            difference = setdiff(file_secids, data_secids)
-            if !isempty(difference)
-                println("secids don't match")
-                println(file)
-                println(difference)
-                println()
-            end
-        end
+    data = loadarrow(joinpath(DIRS.mf.init, "mf-data.arrow"))
 
         data_fundids = union(data_fundids, file_fundids)
         data_secids = union(data_secids, file_secids)
     end
 
     gret_data = CSV.read(joinpath(DIRS.mf.raw, "gross_returns.csv"), DataFrame)
-    println(size(gret_data[4:end, 1:end]|>Matrix))
+    println(count(!ismissing, gret_data[:, 4:end]|>Matrix))
     21330*410
 end
