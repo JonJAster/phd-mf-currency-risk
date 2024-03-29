ecb8ca961434ca084b32e7099dfbb468b3c26ebf
Jon Aster <jonaster.mail@gmail.com>
2024-03-29 10:19:59 +1000

    test code in init mf script

--- src/pipeline/mutual-fund-data/init-mf-data.jl
+++ src/pipeline/mutual-fund-data/init-mf-data.jl
@@ -15,10 +15,53 @@ SKIP_FILES = ["info.csv", "equity_allocation.csv", "non_us_equity_allocation.csv
 function init_mf_data()
     task_start = time()
     mf_data_collection = _read_mf_data()
+    
     mf_data = reduce(
         (l,r)->outerjoin(l,r; on=[:fundid, :secid, :date]),
         mf_data_collection
     )
+
+    ###
+    x = mf_data_collection[2]
+    x[x.fundid .== "FSUSA003JQ" .&& x.date .== Date(2022,11,1), :]
+    gb = groupby(mf_data, [:fundid, :date])
+    fund_init_gret_mean = combine(
+        gb,
+        :gross_returns => mean => :non_missing_fund_gret
+    )
+
+    count(!ismissing, fund_init_gret_mean.non_missing_fund_gret)
+
+    raw_gret = CSV.read(joinpath(DIRS.mf.raw, "gross_returns.csv"), DataFrame)
+
+    gb = groupby(raw_gret, :FundId)
+    fund_raw_gret_mean = combine(
+        gb,
+        propertynames(raw_gret)[4:end] .=> mean
+    )
+
+    unique_raw = Set(round.(fund_raw_gret_mean[:, 2:end] |> Matrix,digits=4))
+
+    unique_init = Set(round.(fund_init_gret_mean.non_missing_fund_gret, digits=4))
+
+    setdiff(unique_raw, unique_init)
+
+    for i in unique_init
+        if !(i in unique_raw)
+            error(i)
+        end
+    end
+
+    9.4965 in unique_init
+
+    fund_init_gret_mean[coalesce.(round.(fund_init_gret_mean.non_missing_fund_gret,digits=4) .== 13.6586,false), :]
+
+    rename!(fund_raw_gret_mean, [:fundid, Symbol.([Date(1990,1,1) + Month(i) for i in 0:size(fund_raw_gret_mean, 2)-2])...])
+
+    fund_raw_gret_mean[fund_raw_gret_mean.fundid .== "FSUSA003JQ", Symbol(Date(2022,11,1))]
+
+    raw_gret[raw_gret.FundId .== "FSUSA003JQ", r"Name|FundId|SecId|.*2022-11.*"]
+    mf_data[mf_data.fundid .== "FSUSA003JQ" .&& mf_data.date .== Date(2022,11,1), :]
 
     drop_allmissing!(mf_data, Not([:fundid, :secid, :date]); dims=:rows)
 
@@ -30,7 +73,7 @@ function _read_mf_data()
     folder = DIRS.mf.raw
     files = readdir(folder, )
     data = DataFrame[]
-    for file in files
+    for file in files # file = "gross_returns.csv"
         file in SKIP_FILES && continue
 
         filepath = joinpath(folder, file)
