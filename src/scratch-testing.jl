4e0cd3dd4dedd121b25664d051e05d6dd079e395
Jon Aster <jonaster.mail@gmail.com>
2024-04-01 06:38:00 +1000

    tests

--- src/scratch-testing.jl
+++ src/scratch-testing.jl
@@ -19,27 +19,5 @@ using .CommonConstants
 using .CommonFunctions
 
 function test()
-    info = CSV.read(joinpath(DIRS.mf.raw, "info.csv"), DataFrame)
-
-    info_fundids = info.FundId |> Set
-    info_secids = info.SecId |> Set
-
-    raw_gret = CSV.read(joinpath(DIRS.mf.raw, "gross_returns.csv"), DataFrame)
-
-    gb = groupby(raw_gret, :FundId)
-    fund_raw_gret = combine(
-        gb,
-        propertynames(raw_gret)[4:end] .=> (x->all(!ismissing, x)) 
-    )
-
-    count(x->x, fund_raw_gret[:, 2:end] |> Matrix)
-
-    data = loadarrow(joinpath(DIRS.mf.init, "mf-data.arrow"))
-
-    1-length(unique(data.fundid))/5145
-    1-length(unique(data.secid))/21333
-    
-    gret_data = CSV.read(joinpath(DIRS.mf.raw, "gross_returns.csv"), DataFrame)
-    println(count(!ismissing, gret_data[:, 4:end]|>Matrix))
-    21330*410
+    factors_data = loadarrow(joinpath(DIRS.combo.factors, "factors.arrow"))
 end
