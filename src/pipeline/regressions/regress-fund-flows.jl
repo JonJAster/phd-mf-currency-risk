a5952371d0400e7532ce1fc4064db6ff01ddf5fc
Jon Aster <jonaster.mail@gmail.com>
2024-04-01 06:06:21 +1000

    remove test comment out of module line

--- src/pipeline/regressions/regress-fund-flows.jl
+++ src/pipeline/regressions/regress-fund-flows.jl
@@ -1,14 +1,14 @@
-#module RegressFundFlows
+module RegressFundFlows
 
-using Revise
+#using Revise
 using DataFrames
 using Arrow
 using Dates
 using GLM
 using Distributions
 
-includet("../../shared/CommonConstants.jl")
-includet("../../shared/CommonFunctions.jl")
+include("../../shared/CommonConstants.jl")
+include("../../shared/CommonFunctions.jl")
 
 using .CommonFunctions
 using .CommonConstants
@@ -119,4 +119,4 @@ if abspath(PROGRAM_FILE) == @__FILE__
     printtime("regressing all flows", task_start; minutes=true)
 end
 
-#end # module RegressFundFlows
+end # module RegressFundFlows
