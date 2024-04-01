3e1f41d11643e95f2e5215b81fbadfb41742dde8
Jon Aster <jonaster.mail@gmail.com>
2024-04-01 11:10:12 +1000

    testing

--- src/scratch-testing.jl
+++ src/scratch-testing.jl
@@ -20,4 +20,15 @@ using .CommonFunctions
 
 function test()
     factors_data = loadarrow(joinpath(DIRS.combo.factors, "factors.arrow"))
+    mf_data = loadarrow(joinpath(DIRS.mf.refined, "mf-excess-returns.arrow"))
+
+    ff_factors = factors_data[startswith.(factors_data.source_id, "ff_"), :]
+    jkp_factors = factors_data[startswith.(factors_data.source_id, "jkp_"), :]
+
+    compare_mkt = innerjoin(
+        ff_factors[ff_factors.factor .== "mkt", Not(:source_id, :factor)],
+        jkp_factors[jkp_factors.factor .== "mkt", Not(:source_id, :factor)];
+        on = :date,
+        renamecols = "_ff" => "_jkp"
+    )
 end
