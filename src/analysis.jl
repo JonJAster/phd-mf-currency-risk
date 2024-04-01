7b81d2b22357592c872208a5a02bece0950a9ede
Jon Aster <jonaster.mail@gmail.com>
2024-04-01 18:09:15 +1000

    estimate decay coefficient sensitivity

--- src/analysis.jl
+++ src/analysis.jl
@@ -20,6 +20,62 @@ using .CommonFunctions
 using .RegressFundFlows
 
 function analysis()
+    # Q1 - Have investors learned to adjust mutual fund returns for equity risk factors?
+    old_sample_betas = regress_fund_flows(
+        "ff_usa_ffc6",
+        filter_by=x->(investment_target_is(x, :usa) .&& bho_dates_only(x))
+    )
+    new_sample_betas = regress_fund_flows(
+        "ff_usa_ffc6",
+        filter_by=x->(investment_target_is(x, :usa) .&& post_bho_only(x))
+    )
+
+    # Q2
+
+    int_usafactor_ffc6 = regress_fund_flows(
+        "ff_usa_ffc6",
+        filter_by=x->(investment_target_is(x, :wld) .&& .!investment_target_is(x, :emg))
+    )
+
+    int_usafactor_ffc6_ver = regress_fund_flows(
+        "ff_usa_ffc6_ver",
+        filter_by=x->(investment_target_is(x, :wld) .&& .!investment_target_is(x, :emg))
+    )
+
+    int_devfactor_ffc6 = regress_fund_flows(
+        "ff_dev_ffc6",
+        filter_by=x->(investment_target_is(x, :wld) .&& .!investment_target_is(x, :emg))
+    )
+
+    int_devfactor_ffc6_ver = regress_fund_flows(
+        "ff_dev_ffc6_ver",
+        filter_by=x->(investment_target_is(x, :wld) .&& .!investment_target_is(x, :emg))
+    )
+
+    # Q3
+
+    usa_usafactor_ffc6 = regress_fund_flows(
+        "ff_usa_ffc6",
+        filter_by=x->(investment_target_is(x, :usa))
+    )
+
+    int_usafactor_ffc6 = regress_fund_flows(
+        "ff_usa_ffc6",
+        filter_by=x->(investment_target_is(x, :wld) .&& .!investment_target_is(x, :emg))
+    )
+
+    int_devfactor_ffc6 = regress_fund_flows(
+        "ff_dev_ffc6",
+        filter_by=x->(investment_target_is(x, :wld) .&& .!investment_target_is(x, :emg))
+    )
+
+    ##
+
+    int_devfactor_capm = regress_fund_flows(
+        "ff_dev_capm",
+        filter_by=x->(investment_target_is(x, :wld) .&& .!investment_target_is(x, :emg))
+    )
+
     regress_fund_flows("ff_usa_capm", filter_by=x->investment_target_is(x, :usa))
     regress_fund_flows("jkp_usa_capm", filter_by=x->investment_target_is(x, :usa))
 
--- src/shared/CommonConstants.jl
+++ src/shared/CommonConstants.jl
@@ -13,7 +13,7 @@ export MODELS
 
 const BETA_LAGS = 60
 const MIN_REGRESSION_OBS = 36
-const DEFAULT_DECAY = 0.186
+const DEFAULT_DECAY = 0.125
 const TIMEWEIGHT_LAGS = 18
 const FLOW_CONTROL_LAGS = 19
 
@@ -35,10 +35,10 @@ const DIRS = (
     ),
     combo = (
         factors = "data/combined/factors",
-        return_betas = "data/combined/return-betas",
-        decomposed = "data/combined/decomposed",
-        weighted = "data/combined/weighted",
-        flow_betas = "data/combined/flow-betas"
+        return_betas = "data/combined/return-betas/decay-125",
+        decomposed = "data/combined/decomposed/decay-125",
+        weighted = "data/combined/weighted/decay-125",
+        flow_betas = "data/combined/flow-betas/decay-125"
     ),
     map = (
         raw = "data/maps/raw",
