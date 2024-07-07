eea0e6a18ce09f59132577d824f3d6b650dde939
Jon Aster <jonaster.mail@gmail.com>
2024-07-07 14:44:08 +1000

    clear out old code

--- src/pipeline/mutual-fund-data/init-mf-data.jl
+++ src/pipeline/mutual-fund-data/init-mf-data.jl
@@ -24,6 +24,7 @@ function _read_mf_timeseries()
     
     mf_ret = loadarrow(joinpath(DIRS.mf.raw, "monthly_returns.arrow"))
     mf_tna = loadarrow(joinpath(DIRS.mf.raw, "monthly_tna.arrow"))
+    
     mf_fees = loadarrow(joinpath(DIRS.mf.raw, "fund_fees.arrow"))
     select!(mf_fees, [:crsp_fundno, :begdt, :enddt, :exp_ratio])
     mf_frontload = loadarrow(joinpath(DIRS.mf.raw, "front_load.arrow"))
@@ -38,182 +39,20 @@ function _read_mf_timeseries()
         [
             :crsp_fundno,
             :chgdt,
-            :enddt,
+            :chgenddt,
             :crsp_cl_grp,
             :index_fund_flag,
-            :vau_fund,
             :et_flag,
-            :dead_flag,
-            :delist_cd,
-            :merge_fundno
+            :retail_fund
         ]
     )
-    mf_fees = _read_mf_fees(wrds, force=true)
+
+    mf_fees_long = _decompress_timeseries(mf_fees)
     
-    mf_data = outerjoin(mf_ret, mf_tna; on=[:crsp_fundno, :caldt])
-    mf_data[!, :exp_ratio] = Array{Union{Missing, Float64}}(missing, nrow(mf_data))
-
-    for row in eachrow(mf_data)
-        row_id = row.crsp_fundno
-        row_date = row.caldt
-        select_fees = mf_fees[mf_fees.crsp_fundno .== row_id, :]
-        select_fees = select_fees[select_fees.begdt .<= row_date, :]
-        select_fees = select_fees[select_fees.enddt .>= row_date, :]
-
-        try
-            (nrow(select_fees) > 0) && (row.exp_ratio = first(select_fees.exp_ratio))
-        catch e
-            println(first(select_fees.exp_ratio))
-            error("stop")
-        end
-    end
-
-    mf_header = _read_mf_header(wrds)
-
-    mf_header
-
-    # mf_fund_style = _read_mf_fund_style(wrds) TODO - activate this
-    close(wrds)
-
-    
-
-
-
-    # folder = DIRS.mf.raw
-    # files = readdir(folder, )
-    # data = DataFrame[]
-    # for file in files
-    #     file in SKIP_FILES && continue
-
-    #     filepath = joinpath(folder, file)
-    #     fieldname = splitext(file)[1]
-        
-    #     data_part = init_raw(filepath)
-    #     data_part_melt = stack(
-    #         data_part, Not([:name, :fundid, :secid]), [:fundid, :secid],
-    #         variable_name=:date, value_name=fieldname
-    #     )
-
-    #     data_part_melt.date = Date.(data_part_melt.date)
-
-    #     push!(data, data_part_melt)
-    # end
     return data
 end
 
-function _read_mf_ret(wrds; limit=nothing, force=false)
-    file_root = "mf-ret"
-    query = """
-        select
-            a.crsp_fundno,
-            a.caldt,
-            a.mret
-        from crsp.monthly_returns a
-    """
 
-    data = _read_mf(wrds, query, file_root; limit=limit, force=force)
-    return data
-end
-
-function _read_mf_tna(wrds; limit=nothing, force=false)
-    file_root = "mf-tna"
-    query = """
-        select
-            a.crsp_fundno,
-            a.caldt,
-            a.mtna
-        from crsp.monthly_tna a
-    """
-
-    data = _read_mf(wrds, query, file_root; limit=limit, force=force)
-    return data
-end
-
-function _read_mf_fees(wrds; limit=nothing, force=false)
-    file_root = "mf-fees"
-    query = """
-        select
-            a.crsp_fundno,
-            a.begdt,
-            a.enddt,
-            a.exp_ratio,
-            a.actual_12b1,
-            a.max_12b1
-        from crsp.fund_fees a
-    """
-
-    data = _read_mf(wrds, query, file_root; limit=limit, force=force)
-    return data
-end
-
-function _read_mf_header(wrds; limit=nothing, force=false)
-    file_root = "mf-header"
-    query = """
-        select
-            a.crsp_fundno,
-            a.crsp_cl_grp,
-            a.fund_name,
-            a.first_offer_dt,
-            a.index_fund_flag,
-            a.vau_fund,
-            a.et_flag,
-            a.dead_flag,
-            a.delist_cd,
-            a.merge_fundno
-        from crsp.fund_hdr a
-    """
-
-    data = _read_mf(wrds, query, file_root; limit=limit, force=force)
-    return data
-end
-
-function _read_mf_fund_style(wrds; limit=nothing, force=false)
-    file_root = "mf-fund-style"
-    query = """
-        select
-            a.crsp_fundno,
-            a.begdt,
-            a.enddt,
-            a.crsp_obj_cd,
-            a.si_obj_cd,
-
-            a.weight
-        from crsp.fund_style a
-    """
-
-    data = _read_mf(wrds, query, file_root; limit=limit, force=force)
-    return data
-end
-
-function _read_mf(wrds, query, file_root; limit=nothing, force=false)
-    """
-    Reads mutual fund data from WRDS.
-
-    Parameters
-    ----------
-    wrds : WRDS
-        WRDS connection object.
-    query : str
-        SQL query to execute.
-    file_root : str
-        Root of the file name to save/load the data to/from disk.
-    limit : Union{Nothing, Int}, optional
-        Number of rows to read. If nothing, reads all rows.
-    force : Bool, optional
-        If true, forces the function to read from WRDS. If false, reads from
-        the local file if it exists.
-    """
-
-    filepath = joinpath(DIRS.mf.raw, "$file_root.arrow")
-
-    if !force && isfile(filepath)
-        data = Arrow.Table(filepath) |> DataFrame
-    else
-        data = query_wrds(wrds, query; limit=limit, save_to=file_root)
-    end
-
-    return data
-end
 
 function _drop_allmissing_funds!(data)
     fund_level_missing_mask = combine(
