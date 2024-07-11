488c8c8d191ed356c4b0d02483d5f2880d34b263
Jon Aster <jonaster.mail@gmail.com>
2024-07-11 14:40:57 +1000

    refactor load_dummies func to proliferate missings

--- src/pipeline/mutual-fund-data/process-mf-data.jl
+++ src/pipeline/mutual-fund-data/process-mf-data.jl
@@ -24,6 +24,7 @@ function process_mf_data()
     _identify_funds!(data)
 
     _add_load_dummies!(data)
+    _
 
     aggregate_data = _aggregate_to_fund_level(data)
     _calculate_fund_flows!(aggregate_data) # TODO: Verify calculation
@@ -87,18 +88,12 @@ function _identify_funds!(data)
 end
 
 function _add_load_dummies!(data)
-    data.pure_no_load = (
-        coalesce.(data.front_load .== 0, true) .&&
-        coalesce.(data.rear_load .== 0, true)
+    data.pure_no_load = (data.front_load .== 0) .&& (data.rear_load .== 0)
+    data.morningstar_no_load = data.pure_no_load .&& coalesce.(
+            data.actual_12b1 .<= 0.0025,
+            data.max_12b1 .<= 0.0025
     )
-
-    data.morningstar_no_load = data.pure_no_load .&& (
-        coalesce.(
-            coalesce.(data.actual_12b1 .<= 0.0025, data.max_12b1 .<= 0.0025),
-            true
-        )
-    )
-
+    
     return data[!, [:pure_no_load, :morningstar_no_load]]
 end
 
