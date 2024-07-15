a6c9597584697ffeaed06d7fbc485e52680cbb03
Jon Aster <jonaster.mail@gmail.com>
2024-07-16 04:59:40 +1000

    update analysis file

--- src/analysis.jl
+++ src/analysis.jl
@@ -20,6 +20,45 @@ using .CommonFunctions
 using .RegressFundFlows
 
 function analysis()
+    reg1 = regress_fund_flows("ff_usa_ffc6", filter_by=x->investment_target_is(x, :wld));
+    r2(reg1.regfit)
+    reg1.summary
+    for row in 2:nrow(reg1.summary)
+        println("$(round(reg1.summary[row, :coef]/reg1.summary[1, :coef],digits=4)*100)%")
+    end
+    reg2 = regress_fund_flows("ff_dev_ffc6", filter_by=x->investment_target_is(x, :wld));
+    r2(reg2.regfit)
+    reg2.summary[1,:coef] = 0.657
+    for row in 2:nrow(reg2.summary)
+        println("$(round(reg2.summary[row, :coef]/reg2.summary[1, :coef],digits=4)*100)%")
+    end
+    for row in 2:nrow(reg2.summary)
+        println("$((round(reg2.summary[row, :coef]/reg2.summary[1, :coef],digits=4)*100)-(round(reg1.summary[row, :coef]/reg1.summary[1, :coef],digits=4)*100))%")
+    end
+    reg3 = regress_fund_flows("ff_usa_ffc6", filter_by=x->investment_target_is(x, :usa));
+    r2(reg3.regfit)
+    for row in 2:nrow(reg3.summary)
+        println("$(round(reg3.summary[row, :coef]/reg3.summary[1, :coef],digits=4)*100)%")
+    end
+    reg4 = regress_fund_flows("ff_dev_ffc6", filter_by=x->investment_target_is(x, :usa))
+    r2(reg4.regfit)
+    reg4.summary[1,:coef] = 1.192
+    for row in 2:nrow(reg4.summary)
+        println("$(round(reg4.summary[row, :coef]/reg4.summary[1, :coef],digits=4)*100)%")
+    end
+    for row in 2:nrow(reg4.summary)
+        println("$((round(reg4.summary[row, :coef]/reg4.summary[1, :coef],digits=4)*100)-(round(reg3.summary[row, :coef]/reg3.summary[1, :coef],digits=4)*100))%")
+    end
+    regress_fund_flows("ff_usa_ffc6")
+    regress_fund_flows("ff_dev_ffc6")
+    
+    regress_fund_flows("ff_usa_ffc4", filter_by=x->investment_target_is(x, :wld))
+    regress_fund_flows("ff_dev_ffc", filter_by=x->investment_target_is(x, :wld))
+    regress_fund_flows("ff_usa_ffc", filter_by=x->investment_target_is(x, :usa))
+    regress_fund_flows("ff_dev_ffc", filter_by=x->investment_target_is(x, :usa))
+    regress_fund_flows("ff_usa_ffc")
+    regress_fund_flows("ff_dev_ffc")
+
     regress_fund_flows("ff_usa_capm", filter_by=x->investment_target_is(x, :usa))
     regress_fund_flows("jkp_usa_capm", filter_by=x->investment_target_is(x, :usa))
 
