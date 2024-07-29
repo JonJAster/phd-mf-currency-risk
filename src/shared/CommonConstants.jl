e66b65921d3b7ae8df03d51d87cd4349d971366a
Jon Aster <jonaster.mail@gmail.com>
2024-07-30 05:31:44 +1000

    simplify models greatly

--- src/shared/CommonConstants.jl
+++ src/shared/CommonConstants.jl
@@ -56,57 +56,15 @@ const EQUITY_LMS_FACTORS = Dict(
 )
 
 const MODELS = OrderedDict(
-    :ff_usa_capm => ("ff_usa", [:mkt]),
-    :ff_usa_capm_lrv => ("ff_usa", [:mkt, :rx, :hml_fx]),
-    :ff_usa_capm_ver => ("ff_usa", [:mkt, :dollar, :carry]),
-    :ff_usa_ff3 => ("ff_usa", [:mkt, :smb, :hml]),
-    :ff_usa_ff3_lrv => ("ff_usa", [:mkt, :smb, :hml, :rx, :hml_fx]),
-    :ff_usa_ff3_ver => ("ff_usa", [:mkt, :smb, :hml, :dollar, :carry]),
-    :ff_usa_ffc6 => ("ff_usa", [:mkt, :smb, :hml, :wml, :rmw, :cma]),
-    :ff_usa_ffc6_lrv => (
-        "ff_usa", [:mkt, :smb, :hml, :wml, :rmw, :cma, :rx, :hml_fx]
-    ),
+    :ff_usa_ffc4 => ("ff_usa", [:mkt, :smb, :hml, :wml]),
+    :ff_usa_ffc6 => ("ff_usa", [:mkt, :smb, :hml, :rmw, :cma, :wml]),
     :ff_usa_ffc6_ver => (
-        "ff_usa", [:mkt, :smb, :hml, :wml, :rmw, :cma, :dollar, :carry]
+        "ff_usa", [:mkt, :smb, :hml, :rmw, :cma, :wml, :dollar, :carry]
     ),
-    :ff_dev_capm => ("ff_dev", [:mkt]),
-    :ff_dev_capm_lrv => ("ff_dev", [:mkt, :rx, :hml_fx]),
-    :ff_dev_capm_ver => ("ff_dev", [:mkt, :dollar, :carry]),
-    :ff_dev_ff3 => ("ff_dev", [:mkt, :smb, :hml]),
-    :ff_dev_ff3_lrv => ("ff_dev", [:mkt, :smb, :hml, :rx, :hml_fx]),
-    :ff_dev_ff3_ver => ("ff_dev", [:mkt, :smb, :hml, :dollar, :carry]),
-    :ff_dev_ffc6 => ("ff_dev", [:mkt, :smb, :hml, :wml, :rmw, :cma]),
-    :ff_dev_ffc6_lrv => (
-        "ff_dev", [:mkt, :smb, :hml, :wml, :rmw, :cma, :rx, :hml_fx]
-    ),
+    :ff_dev_ffc4 => ("ff_dev", [:mkt, :smb, :hml, :wml]),
+    :ff_dev_ffc6 => ("ff_dev", [:mkt, :smb, :hml, :rmw, :cma, :wml]),
     :ff_dev_ffc6_ver => (
-        "ff_dev", [:mkt, :smb, :hml, :wml, :rmw, :cma, :dollar, :carry]
-    ),
-    :jkp_usa_capm => ("jkp_usa", [:mkt]),
-    :jkp_usa_capm_lrv => ("jkp_usa", [:mkt, :rx, :hml_fx]),
-    :jkp_usa_capm_ver => ("jkp_usa", [:mkt, :dollar, :carry]),
-    :jkp_usa_ff3 => ("jkp_usa", [:mkt, :smb, :hml]),
-    :jkp_usa_ff3_lrv => ("jkp_usa", [:mkt, :smb, :hml, :rx, :hml_fx]),
-    :jkp_usa_ff3_ver => ("jkp_usa", [:mkt, :smb, :hml, :dollar, :carry]),
-    :jkp_usa_ffc6 => ("jkp_usa", [:mkt, :smb, :hml, :wml, :rmw, :cma]),
-    :jkp_usa_ffc6_lrv => (
-        "jkp_usa", [:mkt, :smb, :hml, :wml, :rmw, :cma, :rx, :hml_fx]
-    ),
-    :jkp_usa_ffc6_ver => (
-        "jkp_usa", [:mkt, :smb, :hml, :wml, :rmw, :cma, :dollar, :carry]
-    ),
-    :jkp_dev_capm => ("jkp_dev", [:mkt]),
-    :jkp_dev_capm_lrv => ("jkp_dev", [:mkt, :rx, :hml_fx]),
-    :jkp_dev_capm_ver => ("jkp_dev", [:mkt, :dollar, :carry]),
-    :jkp_dev_ff3 => ("jkp_dev", [:mkt, :smb, :hml]),
-    :jkp_dev_ff3_lrv => ("jkp_dev", [:mkt, :smb, :hml, :rx, :hml_fx]),
-    :jkp_dev_ff3_ver => ("jkp_dev", [:mkt, :smb, :hml, :dollar, :carry]),
-    :jkp_dev_ffc6 => ("jkp_dev", [:mkt, :smb, :hml, :wml, :rmw, :cma]),
-    :jkp_dev_ffc6_lrv => (
-        "jkp_dev", [:mkt, :smb, :hml, :wml, :rmw, :cma, :rx, :hml_fx]
-    ),
-    :jkp_dev_ffc6_ver => (
-        "jkp_dev", [:mkt, :smb, :hml, :wml, :rmw, :cma, :dollar, :carry]
+        "ff_dev", [:mkt, :smb, :hml, :rmw, :cma, :wml, :dollar, :carry]
     )
 )
 
