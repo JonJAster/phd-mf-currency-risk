6323955840bdae3c9ba2a8dd7b27d3f389162ff5
Jon Aster <jonaster.mail@gmail.com>
2024-07-30 23:30:20 +1000

    include distributions and begin calc p values

--- src/analysis.jl
+++ src/analysis.jl
@@ -8,6 +8,7 @@ using DataStructures
 using StatsBase
 using Base.Threads
 using LinearAlgebra
+using Distributions
 # using Plots
 using ShiftedArrays: lead, lag
 
@@ -62,8 +63,6 @@ function analysis()
     se_proportion_usa_usa = [
         _delta_se(i, coef_usa_usa, v_usa_usa_coefs) for i in 2:length(coef_usa_usa)
     ]
-<<<<<<< HEAD
-=======
     se_proportion_usa_dev = [
         _delta_se(i, coef_usa_dev, v_usa_dev_coefs) for i in 2:length(coef_usa_dev)
     ]
@@ -73,12 +72,11 @@ function analysis()
     se_proportion_wld_dev = [
         _delta_se(i, coef_wld_dev, v_wld_dev_coefs) for i in 2:length(coef_wld_dev)
     ]
-    )
+
     p_proportion_usa_usa = [
         2 * cdf(TDist(nrow(regout_usa_usa.regfit) - length(coef_usa_usa) - 1), -abs(proportion_coef_usa_usa[i] / se_proportion_usa_usa[i]))
         for i in 1:length(proportion_coef_usa_usa)
     ]
->>>>>>> 7743045 (half finished edit before reverting)
 
 end
  
