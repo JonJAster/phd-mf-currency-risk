570f33204e735485739185b50b1379e6244e3fa0
Jon Aster <jonaster.mail@gmail.com>
2024-07-30 23:19:25 +1000

    use already aggregated info data in filter_fundids

--- src/shared/CommonFunctions.jl
+++ src/shared/CommonFunctions.jl
@@ -380,18 +380,13 @@ function drop_allmissing!(df, cols; dims=1)
 end
 
 function filter_fundids(condition, data)
-    info_filename = joinpath(DIRS.mf.init, "mf-info.arrow")
+    info_filename = joinpath(DIRS.mf.refined, "mf-info.arrow")
     info = loadarrow(info_filename)
 
     info_condition_cols = [
         :global_category, :morningstar_category, :us_category_group, :investment_area
     ]
     select!(info, [:fundid; info_condition_cols])
-
-    # First ensure that the retained fields don't differ for the same fundid before
-    # selecting only the first row for each fundid.
-    assert_similar_fundids(info)
-    info = unique(info, :fundid)
 
     joined_data = innerjoin(data, info, on=:fundid)
     filtered_data = joined_data[condition(joined_data), propertynames(data)]
