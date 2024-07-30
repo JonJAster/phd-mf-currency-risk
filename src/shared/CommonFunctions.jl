b306b7f2e7978a05f60fefe1465bd0ce342354f9
Jon Aster <jonaster.mail@gmail.com>
2024-07-30 13:09:50 +1000

    make assert_similar_fundids public
    (cherry picked from commit 2637d3fadd98e0aa2c29127036f3dbab28554a31)

--- src/shared/CommonFunctions.jl
+++ src/shared/CommonFunctions.jl
@@ -10,6 +10,7 @@ using ShiftedArrays: lead, lag
 include("CommonConstants.jl")
 using .CommonConstants
 
+export assert_similar_fundids
 export dirslist
 export makepath
 export qhead
@@ -39,6 +40,26 @@ const PARAMETER_REGRESSION_ARGS = [
     :plus_lags, :plus_lag, :nth_lags, :nth_lag, :lags, :lag, :time_fixed_effects, :tfe
 ]
 const NOCOLUMN_REGRESSION_ARGS = [:time_fixed_effects, :tfe, :entity_fixed_effects, :efe]
+
+function assert_similar_fundids(info)
+    """
+    Throw an error if a single fundid has at least two distinct values for any field in
+    the input DataFrame.
+    """
+
+    fundids = unique(info.fundid)
+    test_fields = setdiff(propertynames(info), [:fundid])
+
+    for fund in fundids
+        for field in test_fields
+            if length(unique(info[info.fundid .== fund, field])) > 1
+                error("Non-unique $field for fundid $fund.")
+            end
+        end
+    end
+    
+    return
+end
 
 function dirslist()
     println("-- DIRS LIST --")
@@ -361,9 +382,15 @@ end
 function filter_fundids(condition, data)
     info_filename = joinpath(DIRS.mf.init, "mf-info.arrow")
     info = loadarrow(info_filename)
-    select!(info, [:fundid, :global_category, :morningstar_category, :us_category_group, :investment_area])
 
-    _assert_similar_fundids(info)
+    info_condition_cols = [
+        :global_category, :morningstar_category, :us_category_group, :investment_area
+    ]
+    select!(info, [:fundid; info_condition_cols])
+
+    # First ensure that the retained fields don't differ for the same fundid before
+    # selecting only the first row for each fundid.
+    assert_similar_fundids(info)
     info = unique(info, :fundid)
 
     joined_data = innerjoin(data, info, on=:fundid)
@@ -606,26 +633,6 @@ function _null_empty_strings!(df)
             df[!, col] = replace(df[!, col], "" => missing)
         end
     end
-    return
-end
-
-function _assert_similar_fundids(info)
-    """
-    Throw an error if a single fundid has at least two distinct values for any field in
-    the input DataFrame.
-    """
-    
-    fundids = unique(info.fundid)
-    test_fields = setdiff(propertynames(info), [:fundid])
-
-    for fund in fundids
-        for field in test_fields
-            if length(unique(info[info.fundid .== fund, field])) > 1
-                error("Non-unique $field for fundid $fund.")
-            end
-        end
-    end
-    
     return
 end
 
