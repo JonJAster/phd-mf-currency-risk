4438cee4987fd33616628eed561da9614a9951de
Jon Aster <jonaster.mail@gmail.com>
2024-08-16 14:15:01 +1000

    add Revise

--- src/bootstrapped-regressions.jl
+++ src/bootstrapped-regressions.jl
@@ -1,15 +1,25 @@
+using Revise
 using DataFrames
 using Arrow
 
-include("shared/CommonConstants.jl")
-include("shared/CommonFunctions.jl")
-include("pipeline/regressions/regress-fund-flows.jl")
+includet("shared/CommonConstants.jl")
+includet("shared/CommonFunctions.jl")
+includet("pipeline/regressions/regress-fund-flows.jl")
 
 using .CommonConstants
 using .CommonFunctions
 using .RegressFundFlows
 
 function bootstrapped_regressions()
-    n_trials = 100_000
-    MODELS
+    n_trials = 100#_000
+    
+    output_usa_ffc6 = _create_bootstrapped_table("ff_usa_ffc6", n_trials)
+end
+
+function _create_bootstrapped_table(model_name, n_trials)
+    # model_name = "ff_usa_ffc6"; n_trials = 100
+    task_start = time()
+
+    true_regression = regress_fund_flows(model_name; bootstrapped=false)
+
 end
