0b249aecc85920d0974a8b7e2bc39ba747b0d961
Jon Aster <jonaster.mail@gmail.com>
2024-08-16 14:42:45 +1000

    propagate fix

--- src/shared/CommonFunctions.jl
+++ src/shared/CommonFunctions.jl
@@ -170,6 +170,7 @@ function _bootstrap_fund_data(fund_rets_data)
     for (i, fund_i) in enumerate(selected_funds)
         fund_tag = "fund_$i" #TODO update fundids to be symbols universally
         bootstrapped_funds[i] = grouped_data[fund_i]
+        bootstrapped_funds[i][!, :fundid] .= fund_tag
     end
     
     bootstrapped_data = vcat(bootstrapped_funds...)
