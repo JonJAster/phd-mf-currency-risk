90d83429f67d3af598b9f6e135da2f5d0befb873
Jon Aster <jonaster.mail@gmail.com>
2024-08-19 05:29:18 +1000

    try to optimise bootstrapping

--- src/bootstrapped-regressions.jl
+++ src/bootstrapped-regressions.jl
@@ -15,6 +15,9 @@ function bootstrapped_regressions()
     n_trials = 100#_000
     
     output_d = _create_bootstrapped_table(n_trials; filter_by=x->!x.foreign)
+    output_f = _create_bootstrapped_table(n_trials; filter_by=x->x.foreign)
+
+    output_filepath)
 end
 
 function _create_bootstrapped_table(n_trials; filter_by=nothing)
@@ -37,12 +40,18 @@ function _create_bootstrapped_table(n_trials; filter_by=nothing)
     true_regression_usa = regress_fund_flows("ff_usa_ffc6"; filter_by=filter_by, bootstrapped=false).summary
     true_regression_dev = regress_fund_flows("ff_dev_ffc6"; filter_by=filter_by, bootstrapped=false).summary
 
-    bootstrapped_outputs = [copy(coefficient_table) for i in 1:n_trials]
-    for (i, table_i) in enumerate(bootstrapped_outputs)
-        # (i, table_i) = (1, first(bootstrapped_outputs))
+    n_coefficients = nrow(coefficient_table)*(ncol(coefficient_table) - 1)
+    bootstrapped_outputs = Matrix{Float64}(undef, n_coefficients, n_trials)
+    for i in 1:size(bootstrapped_outputs, 2)
+        # i=1
+        col_size = size(bootstrapped_outputs, 1)
         boot_regression_usa = regress_fund_flows("ff_usa_ffc6"; filter_by=filter_by, bootstrapped=true).summary
         boot_regression_dev = regress_fund_flows("ff_dev_ffc6"; filter_by=filter_by, bootstrapped=true).summary
-        _fill_coefficient_table!(table_i, boot_regression_usa, boot_regression_dev)
+        _fill_coefficient_col!(
+            view(bootstrapped_outputs, :, i),
+            boot_regression_usa,
+            boot_regression_dev
+        )
         i % 5 == 0 && printtime("$(i)th bootstrapped regression", task_start)
     end
 
@@ -71,29 +80,31 @@ function _create_bootstrapped_table(n_trials; filter_by=nothing)
     return output
 end
 
-function _fill_coefficient_table!(coefficient_table, regression_usa, regression_dev)
-    # coefficient_table = i; regression_usa = boot_regression_usa; regression_dev = boot_regression_dev
-    coefficient_table.usa_coef = regression_usa.coef
-    coefficient_table.dev_coef = regression_dev.coef
+function _fill_coefficient_col!(coefficient_col, regression_usa, regression_dev)
+    # regression_usa = boot_regression_usa; regression_dev = boot_regression_dev
+    usa_propα = regression_usa.coef / regression_usa.coef[1]
+    dev_propα = regression_dev.coef / regression_dev.coef[1]
+    usa_m_dev = [
+        regression_usa.coef[1] - regression_dev.coef[1];
+        usa_propα[2:end] - dev_propα[2:end]
+    ]
+    
+    coefficient_col .= [
+        regression_usa.coef;
+        regression_dev.coef;
+        usa_propα;
+        dev_propα;
+        usa_m_dev
+    ]
 
-    coefficient_table.usa_propα = coefficient_table.usa_coef / coefficient_table.usa_coef[1]
-    coefficient_table.dev_propα = coefficient_table.dev_coef / coefficient_table.dev_coef[1]
-
-    coefficient_table.dev_m_usa[1] = (
-        coefficient_table.usa_coef[1] - coefficient_table.dev_coef[1]
-    )
-    coefficient_table.dev_m_usa[2:end] = (
-        coefficient_table.usa_propα[2:end] - coefficient_table.dev_propα[2:end]
-    )
-
-    return coefficient_table
+    return coefficient_col
 end
 
 function _fill_bootstrapped_se!(bootstrapped_se, bootstrapped_outputs)
     for i in 1:nrow(bootstrapped_se)
         for j in 1:ncol(bootstrapped_se)
             j == 1 && continue
-            bootstrapped_se[i, j] = std([x[i, j] for x in bootstrapped_outputs])
+            bootstrapped_se[i, j] = std(bootstrapped_outputs[i+j, :])
         end
     end
 end
