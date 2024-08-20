42fbe78f3a5b673c65ac0eb07dadc8758ca2776d
Jon Aster <jonaster.mail@gmail.com>
2024-08-21 04:43:55 +1000

    fix calculation of se

--- src/bootstrapped-regressions.jl
+++ src/bootstrapped-regressions.jl
@@ -13,7 +13,7 @@ using .CommonFunctions
 using .RegressFundFlows
 
 function bootstrapped_regressions()
-    n_trials = 10_0#00
+    n_trials = 10_000
     
     output_d = _create_bootstrapped_table(n_trials; filter_by=x->!x.foreign)
     output_f = _create_bootstrapped_table(n_trials; filter_by=x->x.foreign)
@@ -28,9 +28,6 @@ end
 function _create_bootstrapped_table(n_trials; filter_by=nothing)
     # n_trials = 10; filter_by=x->!x.foreign
 
-    # TODO - the structure of this data is designed to be readable for me, but it is
-    # almost certainly slower than alternatives, like just having a single list of floats
-    # and an index array.
     coefficient_table = DataFrame(
         :factor =>
             [:alpha, :wret_mkt, :wret_smb, :wret_hml, :wret_rmw, :wret_cma, :wret_wml],
@@ -49,6 +46,7 @@ function _create_bootstrapped_table(n_trials; filter_by=nothing)
 
     task_start = time()
     for i in 1:n_trials
+        # i = 1
         col_size = size(bootstrapped_outputs, 1)
         boot_regression_usa = regress_fund_flows("ff_usa_ffc6"; filter_by=filter_by, bootstrapped=true).summary
         boot_regression_dev = regress_fund_flows("ff_dev_ffc6"; filter_by=filter_by, bootstrapped=true).summary
@@ -124,10 +122,11 @@ function _fill_coefficient_table!(coefficient_table, regression_usa, regression_
 end
 
 function _fill_bootstrapped_se!(bootstrapped_se, bootstrapped_outputs)
-    for i in 1:nrow(bootstrapped_se)
-        for j in 1:ncol(bootstrapped_se)
-            j == 1 && continue
-            bootstrapped_se[i, j] = std(bootstrapped_outputs[i+j, :])
+    for i in 1:ncol(bootstrapped_se)
+        i == 1 && continue
+        for j in 1:nrow(bootstrapped_se)
+            idx = (i - 2)*nrow(bootstrapped_se) + j
+            bootstrapped_se[i, j] = std(bootstrapped_outputs[idx, :])
         end
     end
 end
@@ -161,4 +160,10 @@ function _format_output_column(coef, se)
         "$(round(coef[i], digits=3)) ± $(round(se[i], digits=3))"
         for i in 1:length(coef)
     ]
+end
+
+if isnothing(match(r"terminalserver.jl$", abspath(PROGRAM_FILE)))
+    task_start = time()
+    bootstrapped_regressions()
+    printtime("bootstrapping regressions", task_start)
 end
