17a84e89fda6b1b9e58a342274a21ed580ce1c98
Jon Aster <jonaster.mail@gmail.com>
2024-08-22 15:21:13 +1000

    fix error

--- src/bootstrapped-regressions.jl
+++ src/bootstrapped-regressions.jl
@@ -237,7 +237,8 @@ function _fill_output_table!(
     output.dev_m_usa[1:end-4] = _format_output_column(
         true_coefficient_table.dev_m_usa, bootstrapped_se.dev_m_usa
     )
-    output.dev_m_usa[end-3:end] = _format_output_column(
+    output.dev_m_usa[end-3:end-2] .= ""
+    output.dev_m_usa[end-1:end] = _format_output_column(
         [sum_row[1, :dev_m_usa]], [sum_row[2, :dev_m_usa]]
     )
 
