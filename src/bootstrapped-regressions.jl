4dc0dc276b4d2b29dbaab4f9b4e1c904536a9dd5
Jon Aster <jonaster.mail@gmail.com>
2024-08-29 20:15:56 +1000

    fix typo

--- src/bootstrapped-regressions.jl
+++ src/bootstrapped-regressions.jl
@@ -638,7 +638,7 @@ function _fill_output_table_dated!(
     sum_row
 )
 # early_coef
-output_early_coef[1] = _format_output_column(
+output.early_coef[1] = _format_output_column(
     [true_coefficient_table.late_coef[1]], [true_se.late_se[1]]
 )
 output.early_coef[2:end-2] = _format_output_column(
