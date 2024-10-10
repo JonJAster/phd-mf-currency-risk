d4c20c8c0f49c21d9c34a397d5fd224d7db6d81b
Jon Aster <jonaster.mail@gmail.com>
2024-10-10 11:39:16 +1000

    clearing old tests

--- src/scratch-testing.jl
+++ src/scratch-testing.jl
@@ -24,6 +24,38 @@ using .CommonFunctions
 using .RegressFundFlows
 
 function test()
+    tw_ret_file = joinpath(DIRS.combo.weighted, "ff_usa_ffc6.arrow")
+    tw_ret = loadarrow(tw_ret_file)
+
+    tw_ret_file_dev = joinpath(DIRS.combo.weighted, "ff_dev_ffc6.arrow")
+    tw_ret_dev = loadarrow(tw_ret_file_dev)
+
+    minimum(tw_ret.date)
+    minimum(tw_ret_dev.date)
+
+    factors_file = joinpath(DIRS.eq.factors, "ff.arrow")
+    factors = loadarrow(factors_file)
+
+    factors_usa = factors[factors.source_id .== "ff_usa", :]
+    factors_dev = factors[factors.source_id .== "ff_dev", :]
+
+    println("USA ", maximum([minimum(factors_usa[factors_usa.factor .== f, :date]) for f in unique(factors_usa.factor)]), " ", minimum([maximum(factors_usa[factors_usa.factor .== f, :date]) for f in unique(factors_usa.factor)]))
+    for f in unique(factors_usa.factor)
+        println(f, " ", minimum(factors_usa[factors_usa.factor .== f, :date]), " ", maximum(factors_usa[factors_usa.factor .== f, :date]))
+    end
+    
+    println()
+    println("DEV ", maximum([minimum(factors_dev[factors_dev.factor .== f, :date]) for f in unique(factors_dev.factor)]), " ", minimum([maximum(factors_dev[factors_dev.factor .== f, :date]) for f in unique(factors_dev.factor)]))
+    for f in unique(factors_dev.factor)
+        println(f, " ", minimum(factors_dev[factors_dev.factor .== f, :date]), " ", maximum(factors_dev[factors_dev.factor .== f, :date]))
+    end
+
+    betas_file_usa = joinpath(DIRS.combo.return_betas, "ff_usa_ffc6.arrow")
+    betas_usa = loadarrow(betas_file)
+    be
+
+
+
     factors_file = joinpath(DIRS.eq.factors, "ff.arrow")
     factors = loadarrow(factors_file)
 
